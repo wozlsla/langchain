a9d5d63d1e31b28c0275fa254ce2bf09f155cde7
jimin <jiminseo0307@gmail.com>
2024-10-01 00:02:11 +0900

    7.7 Chat History

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -12,8 +12,8 @@ st.set_page_config(
 )
 
 
+@st.cache_data(show_spinner="Embedding file...")
 def embed_file(file):
-
     file_content = file.read()
     file_path = f"./.cache/files/{file.name}"
     with open(file_path, "wb") as f:
@@ -29,11 +29,25 @@ def embed_file(file):
     docs = loader.load_and_split(text_splitter=splitter)
     embeddings = OpenAIEmbeddings()
     cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
-
     vectorstore = FAISS.from_documents(docs, cached_embeddings)
     retriever = vectorstore.as_retriever()
-
     return retriever
+
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        st.session_state["messages"].append({"message": message, "role": role})
+
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(
+            message["message"],
+            message["role"],
+            save=False,
+        )
 
 
 st.title("DocumentGPT")
@@ -43,15 +57,25 @@ st.markdown(
 Welcome!
             
 Use this chatbot to ask questions to an AI about your files!
+
+Upload your files on the sidebar.
 """
 )
 
-file = st.file_uploader(
-    "Upload a .txt .pdf or .docx file",
-    type=["pdf", "txt", "doc"],
-)
+with st.sidebar:
+    file = st.file_uploader(
+        "Upload a .txt .pdf or .docx file",
+        type=["pdf", "txt", "doc"],
+    )
 
 if file:
     retriever = embed_file(file)
-    s = retriever.invoke("winstom")
-    s
+
+    send_message("I'm ready! Ask away!", "ai", save=False)
+    paint_history()
+    message = st.chat_input("Ask anything about your file..")
+    if message:
+        send_message(message, "human")
+        send_message("hey go to bed", "ai")
+else:
+    st.session_state["messages"] = []
