c5d8a0db4c69898259df96b5a13c1311b1f7f791
jimin <jiminseo0307@gmail.com>
2024-09-30 15:25:58 +0900

    7.4 Chat Messages

--- Home.py
+++ Home.py
@@ -5,4 +5,19 @@ st.set_page_config(
     page_icon="🍊",
 )
 
-st.title("FullstackGPT Home")
+st.markdown(
+    """
+# Hello!
+            
+Welcome to my FullstackGPT Portfolio!
+            
+Here are the apps I made:
+            
+- [ ] [DocumentGPT](/DocumentGPT)
+- [ ] [PrivateGPT](/PrivateGPT)
+- [ ] [QuizGPT](/QuizGPT)
+- [ ] [SiteGPT](/SiteGPT)
+- [ ] [MeetingGPT](/MeetingGPT)
+- [ ] [InvestorGPT](/InvestorGPT)
+"""
+)
