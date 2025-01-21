d29e47fc6846979d813bf504f027de555439eea3
jimin <jiminseo0307@gmail.com>
2025-01-21 20:51:36 +0900

    feat: Site GPT with SitemapLoader and Map ReRank chain

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,14 +1,165 @@
-# playwright install
-from langchain.document_loaders import AsyncChromiumLoader
-from langchain.document_transformers import Html2TextTransformer
+from unittest import result
+from langchain.document_loaders import SitemapLoader
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.callbacks import StreamingStdOutCallbackHandler
 import streamlit as st
+
+
+answer_prompt = ChatPromptTemplate.from_template(
+    """
+    Using ONLY the following context answer the user's question. If you can't just say you don't know, don't make anything up.
+                                                  
+    Then, give a score to the answer between 0 and 5.
+
+    If the answer answers the user question the score should be high, else it should be low.
+
+    Make sure to always include the answer's score even if it's 0.
+
+    Context: {context}
+                                                  
+    Examples:
+                                                  
+    Question: How far away is the moon?
+    Answer: The moon is 384,400 km away.
+    Score: 5
+                                                  
+    Question: How far away is the sun?
+    Answer: I don't know
+    Score: 0
+                                                  
+    Your turn!
+
+    Question: {question}
+    """
+)
+
+
+def get_answers(inputs):
+    docs = inputs["docs"]
+    question = inputs["question"]
+    answer_chain = answer_prompt | llm
+    # a = {
+    #     "question": question,
+    #     "answer": [
+    #         {
+    #             "answer": answer_chain.invoke(
+    #                 {"question": question, "context": doc.page_content}
+    #             ).content,
+    #             "source": doc.metadata["source"],
+    #             "date": doc.metadata["lastmod"],
+    #         }
+    #         for doc in docs
+    #     ],
+    # }
+    # st.write(a["answer"])
+    return {
+        "question": question,
+        "answer": [
+            {
+                "answer": answer_chain.invoke(
+                    {"question": question, "context": doc.page_content}
+                ).content,
+                "source": doc.metadata["source"],
+                "date": doc.metadata["lastmod"],
+            }
+            for doc in docs
+        ],
+    }
+
+
+choose_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Use ONLY the following pre-existing answers to answer the user's question.
+
+            Use the 'Answer' to answers that have the highest 'Score' (more helpful) and favor the most recent ones.
+
+            Cite sources and return the sources of the answers as they are, do not change them.
+            
+            Choose the most informed answer among the answers with the same score.
+
+            You should always respond to the source.
+
+            Answers: {answers}
+            ---
+            Examples:
+                                                  
+            The moon is 384,400 km away.
+
+            Source: https://example.com
+            """,
+        ),
+        ("human", "{question}"),
+    ]
+)
+
+
+def choose_answer(inputs):
+    answers = inputs["answer"]
+    question = inputs["question"]
+    choose_chain = choose_prompt | llm
+    condensed = "\n\n".join(
+        f"{answer['answer']}\nSource:{answer['source']}\nDate:{answer['date']}\n"
+        for answer in answers
+    )
+    # st.write(condensed)
+    return choose_chain.invoke(
+        {
+            "question": question,
+            "answers": condensed,
+        }
+    )
+
+
+def parse_page(soup):
+    header = soup.find("header")
+    footer = soup.find("footer")
+    if header:
+        header.decompose()
+    if footer:
+        footer.decompose()
+    return (
+        str(soup.get_text())
+        .replace("\n", " ")
+        .replace("\xa0", " ")
+        # .replace("CloseSearch Submit Blog", "")
+    )
+
+
+@st.cache_data(show_spinner="Loading website...")
+def load_website(url):
+    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=1000,
+        chunk_overlap=200,
+    )
+    loader = SitemapLoader(
+        url,
+        # filter_urls=[r"^(?!.*\/blog\/).*"],
+        filter_urls=[
+            r"(.*\/ai-gateway\/).*",
+            r"(.*\/vectorize\/).*",
+            r"(.*\/workers-ai\/).*",
+        ],
+        parsing_function=parse_page,
+    )
+    loader.requests_per_second = 2
+    docs = loader.load_and_split(text_splitter=splitter)
+    vector_store = FAISS.from_documents(docs, OpenAIEmbeddings())
+    return vector_store.as_retriever()
+
 
 st.set_page_config(
     page_title="SiteGPT",
     page_icon="./files/ham.ico",
 )
 
-html2text_transformer = Html2TextTransformer()
 
 st.markdown(
     """
@@ -20,13 +171,40 @@ st.markdown(
 """
 )
 
+
 with st.sidebar:
+
+    llm = ChatOpenAI(
+        temperature=0.1,
+        streaming=True,
+        callbacks=[
+            StreamingStdOutCallbackHandler(),
+        ],
+    )
+
     url = st.text_input(
         "Write down a URL",
         placeholder="https://example.com",
     )
+
+
 if url:
-    loader = AsyncChromiumLoader([url])
-    docs = loader.load()
-    transformed = html2text_transformer.transform_documents(docs)
-    st.write(docs)
+    if ".xml" not in url:
+        with st.sidebar:
+            st.error("Please write down a Sitemap URL.")
+    else:
+        retriever = load_website(url)
+        query = st.text_input("Ask a question to the website.")
+
+        if query:
+            chain = (
+                {
+                    "docs": retriever,
+                    "question": RunnablePassthrough(),
+                }
+                | RunnableLambda(get_answers)
+                | RunnableLambda(choose_answer)
+            )
+
+            result = chain.invoke(query)
+            st.markdown(result.content)
