b98a5246d2d790c47f6285e5ac56bcb8a8028433
jimin <jiminseo0307@gmail.com>
2025-04-10 19:15:28 +0900

    feat: add memory and paint chat history
    issue: summarizing spinner need to removed

--- pages/05_MeetingGPT.py
+++ pages/05_MeetingGPT.py
@@ -2,9 +2,7 @@
 import os
 import math
 import glob
-from pydoc import doc
 import subprocess
-from altair import Text
 import openai  # llm (whisper)
 import streamlit as st
 from pydub import AudioSegment
@@ -22,11 +20,39 @@ from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
 from langchain.storage import LocalFileStore
 from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 
+# Memory
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import MessagesPlaceholder
+from operator import itemgetter
+from langchain.callbacks.base import BaseCallbackHandler
+
+
+class ChatCallbackHandler(BaseCallbackHandler):
+    def __init__(self, *args, **kwargs):
+        self.message = ""
+
+    def on_llm_start(self, *args, **kwargs):
+        self.message_box = st.empty()
+
+    def on_llm_end(self, *args, **kwargs):
+        save_message(self.message, "ai")
+
+    def on_llm_new_token(self, token, *args, **kwargs):
+        self.message += token
+        self.message_box.markdown(self.message)
+
 
 llm = ChatOpenAI(
     model="gpt-4o-mini",
     temperature=0.1,
 )
+
+streaming_llm = ChatOpenAI(
+    temperature=0.1,
+    streaming=True,
+    callbacks=[ChatCallbackHandler()],
+)
+
 
 # for cache (dev env)
 has_transcript = os.path.exists("./.cache/podcast.txt")
@@ -35,6 +61,81 @@ splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
     chunk_size=800,
     chunk_overlap=100,
 )
+
+
+def save_message(message, role):
+    st.session_state["messages"].append({"message": message, "role": role})
+
+
+def save_memory(input, output):
+    st.session_state["memory"].save_context({"input": input}, {"output": output})
+
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        save_message(message, role)
+
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(message["message"], message["role"], save=False)
+
+
+def invoke_chain(question):
+    result = qa_chain.invoke(question)
+    save_memory(question, result.content)
+
+
+# summary cache
+@st.cache_data(show_spinner=False)
+def generate_summary(transcript_path):
+    loader = TextLoader(transcript_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    # st.write(len(docs)) # 25 -> 12 줄임
+
+    # Chain
+    first_summary_prompt = ChatPromptTemplate.from_template(
+        """
+        Write a concise summary of the following:
+        "{text}"
+        CONCISE SUMMARY:
+        """
+    )
+
+    first_summary_chain = first_summary_prompt | llm | StrOutputParser()
+
+    summary = first_summary_chain.invoke({"text": docs[0].page_content})
+
+    refine_prompt = ChatPromptTemplate.from_template(
+        """
+        당신의 역할은 최종 요약본을 작성하는 것입니다.
+        다음은 현재까지 생성된 요약입니다: {existing_summary}
+        추가적인 문맥이 아래에 제공됩니다. 이 문맥이 유용하다고 판단될 경우, 이를 반영하여 기존 요약을 개선하세요.
+        ------------
+        {context}
+        ------------
+        새 문맥이 기존 요약을 더 나아지게 만들 수 없다면, 원래 요약을 그대로 유지하세요.
+        """
+    )
+
+    refine_chain = refine_prompt | llm | StrOutputParser()
+
+    with st.status("Summarizing...") as status:
+        # execpt first summary
+        for i, doc in enumerate(docs[1:]):
+            status.update(label=f"Processing document {i+1}/{len(docs)-1}")
+            # update(override) summary
+            summary = refine_chain.invoke(
+                {
+                    "existing_summary": summary,
+                    "context": doc.page_content,
+                }
+            )
+            # st.write(summary) # 별 내용 없으면 요약이 없을 수도 있음.
+    # final summary
+    return summary
 
 
 @st.cache_data()
@@ -74,7 +175,15 @@ def extract_audio_from_video(video_path):
     if has_transcript:
         return
 
-    audio_path = video_path.replace("mp4", "mp3")
+    audio_path = (
+        video_path.replace(".mp4", ".mp3")
+        .replace(".avi", ".mp3")
+        .replace(".mkv", ".mp3")
+        .replace(".mov", ".mp3")
+    )
+    # if os.path.exists(audio_path):
+    #     return
+
     command = [
         "ffmpeg",
         "-y",  # override -y flag
@@ -114,9 +223,7 @@ st.markdown(
     """
     # MettingGPT
 
-    Upload a video then give you a transcript, a summary and a chat bot to ask any questions about it.
-
-    Get started by uploading a video file in the sidebar.
+    사이드바에서 동영상을 업로드하면 대화의 요약과 대화에 대한 질문을 할 수 있는 챗봇을 제공해 드립니다.
     """
 )
 
@@ -161,88 +268,67 @@ if video:
             st.write(file.read())
 
     with summary_tab:
-        start = st.button("Generate summary")
+        if "isSummaryGenerated" not in st.session_state:
+            st.session_state["isSummaryGenerated"] = False
+        if "summary" not in st.session_state:
+            st.session_state["summary"] = ""
 
-        if start:
-            loader = TextLoader(transcript_path)
-
-            docs = loader.load_and_split(text_splitter=splitter)
-            # st.write(len(docs)) # 25 -> 줄일것
-
-            # Chain
-            first_summary_prompt = ChatPromptTemplate.from_template(
-                """
-                 Write a concise summary of the following:
-                 "{text}"
-                 CONCISE SUMMARY:
-                """
-            )
-
-            first_summary_chain = first_summary_prompt | llm | StrOutputParser()
-
-            summary = first_summary_chain.invoke({"text": docs[0].page_content})
-
-            refine_prompt = ChatPromptTemplate.from_template(
-                """
-                 Your job is to produce a final summary.
-                 We have provided an existing summary up to a certain point: {existing_summary}
-                 We have the opportunity to refine the existing summary (only if needed) with some more context below.
-                 ------------
-                 {context}
-                 ------------
-                 Given the new context, refine the original summary.
-                 If the context isn't useful, RETURN the original summary.
-                 """
-            )
-
-            refine_chain = refine_prompt | llm | StrOutputParser()
-
-            with st.status("Summarizing...") as status:
-                # execpt first summary
-                for i, doc in enumerate(docs[1:]):
-                    status.update(label=f"Processing document {i+1}/{len(docs)-1}")
-                    # update(override) summary
-                    summary = refine_chain.invoke(
-                        {
-                            "existing_summary": summary,
-                            "context": doc.page_content,
-                        }
-                    )
-                    # st.write(summary) # 별 내용 없으면 요약이 없을 수도 있음.
-            # final summary
-            st.write(summary)
+        if st.button("Generate summary"):
+            if st.session_state["isSummaryGenerated"]:
+                st.write(st.session_state["summary"])
+            else:
+                summary = generate_summary(transcript_path)
+                st.session_state["summary"] = summary
+                st.session_state["isSummaryGenerated"] = True
+                st.write(summary)
 
     with qa_tab:
         retriever = embed_file(transcript_path)
-        question = st.text_input("Do you have any questions about this video?")
-        docs = retriever.invoke(question)
+        question = st.text_input("회의에서 일어난 궁금한 일들을 물어보세요.")
 
-        # st.write(docs)
+        paint_history()
+
         qa_prompt = ChatPromptTemplate.from_messages(
             [
                 (
                     "system",
                     """
-                    Your job is to answer the user's question using the given context.
-                    The context is a transcript from a video.
-                    Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.
+                    당신은 회의의 대본 기록을 이용해 사용자의 질문에 답변하는 전문적인 AI입니다.
+                    당신이 기존에 알고 있던 지식을 사용하지 마세요.
+                    모르는 내용이라면 모른다고 하고 지어내지 마세요.
+                    사용자의 질문은 보통 회의에 대해 질문 하는 것이니 당신의 생각을 이야기 하지 마세요.
                     
-                    Context: {context}
+                    대본: {context}
                     """,
                 ),
+                MessagesPlaceholder(variable_name="history"),
                 ("human", "{question}"),
             ]
         )
 
-        qa_chain = (
-            {
-                "context": retriever | RunnableLambda(format_docs),
-                "question": RunnablePassthrough(),
-            }
-            | qa_prompt
-            | llm
-            | StrOutputParser()
-        )
+        if question:
+            send_message(question, "human")
+            qa_chain = (
+                {
+                    "context": retriever | RunnableLambda(format_docs),
+                    "question": RunnablePassthrough(),
+                    "history": RunnableLambda(
+                        st.session_state.memory.load_memory_variables
+                    )
+                    | itemgetter("history"),
+                }
+                | qa_prompt
+                | streaming_llm
+            )
 
-        answer = qa_chain.invoke(question)
-        st.write(answer)
+            with st.chat_message("ai"):
+                invoke_chain(question)
+
+else:
+    st.session_state["messages"] = []
+    st.session_state["memory"] = ConversationBufferMemory(
+        llm=llm,
+        max_token_limit=1000,
+        return_messages=True,
+    )
+    st.session_state["isSummaryGenerated"] = False
